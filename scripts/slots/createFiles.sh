0adc3c71453982facbe169ce1e5aa27f5b8cd96b
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-12-22 16:03:58 +0100

    feature(scripts) : [UNLOCK SLOTS] enhance script to create rim files in release directory

--- scripts/slots/createFiles.sh
+++ scripts/slots/createFiles.sh
@@ -8,8 +8,10 @@ source ../setEnv.sh
 DEFAULT_MODELS_PATH=${TDUCP_PATH}/resources/physics/vehicles/models
 DEFAULT_SOUND_PATH=${TDUCP_PATH}/resources/physics/vehicles/sounds
 DEFAULT_HUD_PATH=${TDUCP_PATH}/resources/physics/vehicles/huds
+DEFAULT_RIM_PATH=${TDUCP_PATH}/resources/physics/vehicles/rims
 
 BUILD_MODELS_PATH=${BUILD_BANK_PATH}/Vehicules
+BUILD_RIMS_PATH=${BUILD_MODELS_PATH}/Rims
 BUILD_SOUNDS_PATH=${BUILD_BANK_PATH}/Sound/Vehicules
 BUILD_HUDS_HR_PATH=${BUILD_BANK_PATH}/FrontEnd/HiRes/Gauges
 BUILD_HUDS_LR_PATH=${BUILD_BANK_PATH}/FrontEnd/LoRes/Gauges
@@ -32,4 +34,13 @@ do
    echo "-${name}"
    cp ${DEFAULT_HUD_PATH}/DEFAULT.bnk ${BUILD_HUDS_HR_PATH}/${name}.bnk
    cp ${DEFAULT_HUD_PATH}/DEFAULT.bnk ${BUILD_HUDS_LR_PATH}/${name}.bnk
+done
+
+echo "-> Creating rim files..."
+cat rims.txt | while read name
+do
+   echo "-${name}"
+   PARENT_PATH=`dirname ${BUILD_RIMS_PATH}/${name}`
+   mkdir -p ${PARENT_PATH}
+   cp ${DEFAULT_RIM_PATH}/DEFAULT.bnk ${BUILD_RIMS_PATH}/${name}.bnk
 done
