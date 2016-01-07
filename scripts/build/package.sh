3c8001932198cf9b701e47a9833a9625fb696376
Jerome CANLER <jerome.canler@ext.pmu.fr>
2016-01-07 10:33:43 +0100

    refactor(scripts) : [] change output format + introduce timestamp in zip file name + rebuild database

--- scripts/build/package.sh
+++ scripts/build/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #set -x
 
-# Prepares packaging of Community Patch.
-# Will use generated Database banks in $GENERATED_DB_PATH directory
+# Prepares packaging of Community Patch
+# Does not include all files yet (to be copied manually to BUILD PATH before launching script).
 
 source ../setEnv.sh
 
@@ -10,23 +10,31 @@ BUILD_PATH=${TDUCP_PATH}/workspace/build/private/2.00A
 RELEASE_PATH=${TDUCP_PATH}/releases
 
 # Manifests
-echo "-> Manifests..."
+echo "*** Manifests... ***"
 cp ${TDUCP_PATH}/manifests/*.md ${BUILD_PATH}
+echo
 
 # Generate mapping
-echo "-> Bnk1.map..."
+echo "*** Bnk1.map... ***"
 cd ../mapping/
 ./generateCurrent.sh
 cd - > /dev/null
+echo
 
 # Database
-echo "-> Database..."
+echo "*** Database... ***"
+cd ../database/
+./generateCurrent.sh
+cd - > /dev/null
 TARGET_DB_PATH=${BUILD_PATH}/Euro/Bnk/Database
 mkdir -p ${TARGET_DB_PATH}
 cp ${GENERATED_DB_PATH}/*.bnk ${TARGET_DB_PATH}
+echo
 
 #Zip
-echo "-> Zipping..."
+echo "*** Zipping... ***"
 pushd ${BUILD_PATH} > /dev/null
-zip -r ${TDUCP_PATH}/workspace/releases/TDUCP-2.00A-NEW.zip *
+TIMESTAMP=`date +%s`
+zip -r ${TDUCP_PATH}/workspace/releases/TDUCP-2.00A-${TIMESTAMP}.zip *
 popd > /dev/null
+echo
