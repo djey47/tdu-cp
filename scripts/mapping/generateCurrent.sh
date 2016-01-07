7f8a3f71c47a443541db90b0a1db76cf1f410952
Jerome CANLER <jerome.canler@ext.pmu.fr>
2016-01-07 10:32:02 +0100

    refactor(scripts) : [] change output format

--- scripts/mapping/generateCurrent.sh
+++ scripts/mapping/generateCurrent.sh
@@ -8,5 +8,5 @@ source ../setEnv.sh
 
 cd ${TDUF_PATH}
 
-echo "-> Generating ${MAPPING_CURRENT} file from ${JSON_MAPPING_PATH}, please wait..."
+echo "Generating ${MAPPING_CURRENT} file from ${JSON_MAPPING_PATH}, please wait..."
 java -cp ./lib/tduf-cli-all-${TDUF_VERSION}.jar fr.tduf.cli.tools.FileTool applyjson -n -i ${JSON_MAPPING_PATH} -o ${MAPPING_CURRENT} -s ${JSON_MAP4_STRUCTURE}
