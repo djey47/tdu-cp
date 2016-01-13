f03472ba32005bc77cd5a1f868055eb75909704f
Jerome CANLER <jerome.canler@ext.pmu.fr>
2016-01-13 14:13:46 +0100

    feature(scripts) : [] introduce scripts to launch TDUF tools from CLI

---
+++ scripts/tduf/databaseTool.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Runs TDUF's DatabaseTool. Please run setEnv.sh script to initialize environment before launching this script.
+
+cd ${TDUF_PATH}
+java -cp ./lib/tduf-cli-all-${TDUF_VERSION}.jar fr.tduf.cli.tools.DatabaseTool $*
+cd - > /dev/null
