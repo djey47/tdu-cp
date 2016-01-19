8b9c14655f225845e9a03bd8242a47ea458f7f82
djey <j.canler@gmail.com>
2016-01-19 13:32:25 +0100

    bugfix(scripts) : [] handle arguments containing spaces

--- scripts/tduf/fileTool.sh
+++ scripts/tduf/fileTool.sh
@@ -3,5 +3,5 @@
 # Runs TDUF's FileTool. Please run setEnv.sh script to initialize environment before launching this script.
 
 cd ${TDUF_PATH}
-java -cp ./lib/tduf-cli-all-${TDUF_VERSION}.jar fr.tduf.cli.tools.FileTool $*
+java -cp ./lib/tduf-cli-all-${TDUF_VERSION}.jar fr.tduf.cli.tools.FileTool "$@"
 cd - > /dev/null
