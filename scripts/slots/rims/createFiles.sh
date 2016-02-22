9d6c2bac3101c1c50874d099720b6967ac082817
djey <j.canler@gmail.com>
2016-02-22 18:30:03 +0100

    feature(mapping) : [OWN RIMS] add rim files for Nissan 350Zs - WIP

--- scripts/slots/rims/createFiles.sh
+++ scripts/slots/rims/createFiles.sh
@@ -21,4 +21,12 @@ do
    mkdir -p "${PARENT_PATH}"
    cp ${DEFAULT_RIM_PATH}/ALFAGT.bnk "${BUILD_RIMS_PATH}/${name}.bnk"
 done
+
+cat rims_nissan.txt | while read name
+do
+   echo "-${name}"
+   PARENT_PATH=`dirname "${BUILD_RIMS_PATH}/${name}"`
+   mkdir -p "${PARENT_PATH}"
+   cp ${DEFAULT_RIM_PATH}/NISSAN350Z.bnk "${BUILD_RIMS_PATH}/${name}.bnk"
+done
 echo
