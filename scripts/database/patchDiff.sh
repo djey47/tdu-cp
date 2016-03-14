a0947e55db8b6555577b3d92aafc9277787f86f8
djey <j.canler@gmail.com>
2016-03-14 17:50:00 +0100

    feature(scripts) : [UPDATE] adapt script to use new apply-patches operation from TDUF 1.3.0

--- scripts/database/patchDiff.sh
+++ scripts/database/patchDiff.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 #set -x
 
-# Applies all full update mini matches onto current JSON database
+# Applies all full update mini matches onto current JSON database. Requires TDUF >= 1.3.0
 
 source ../setEnv.sh
 
 echo "Applying update mini patches to ${CURRENT_DB_PATH}, please wait..."
-find ${DIFF_PATCHES_PATH} -name *.json | sort | while read PATCH_FILE
-do
-    echo "-> Mini patch: ${PATCH_FILE}"
-    ../tduf/databaseTool.sh apply-patch -n -j "${CURRENT_DB_PATH}" -o "${CURRENT_DB_PATH}" -p "${PATCH_FILE}"
-    echo
-done
+../tduf/databaseTool.sh apply-patches -n -j "${CURRENT_DB_PATH}" -o "${CURRENT_DB_PATH}" -p "${DIFF_PATCHES_PATH}"
