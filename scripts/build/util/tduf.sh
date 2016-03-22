36c710b3fe57b5c973fc3d2dd2c95c5dfdfb655f
djey <j.canler@gmail.com>
2016-03-22 13:41:47 +0100

    feature(scripts) : [UPDATE] add script to include TDUF in update build

--- scripts/build/util/tduf.sh
+++ scripts/build/util/tduf.sh
@@ -2,6 +2,6 @@
 
 deployTDUF() {
     echo "*** TDUF... ***"
-    echo "Manual step: extract latest TDUF release onto ${INSTALLER_TDUF_PATH} directory - ignore archive root."
+    cp -r ${TDUF_PATH} ${INSTALLER_PATH}
     echo
 }
