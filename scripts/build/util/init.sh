07c3efd2f129ab0ffc1ccbecb5a641a9558fb2cc
djey <j.canler@gmail.com>
2016-03-23 16:16:41 +0100

    bugfix(scripts) : [] fix directory creation

--- scripts/build/util/init.sh
+++ scripts/build/util/init.sh
@@ -3,26 +3,25 @@
 makeDirectories() {
     echo "*** Preparing directories... ***"
     rm -rf ${BUILD_PATH}
-    mkdir -p ${BUILD_PATH}
     mkdir -p ${BUILD_TOOLS_PATH}
     mkdir -p ${BUILD_DB_PATH}
     mkdir -p ${BUILD_FX_PATH}
     mkdir -p ${RELEASE_PATH}
-    echo
 }
 
 makeDirectoriesForUpdate() {
     makeDirectories
-#    mkdir -p ${INSTALLER_PATH}
     mkdir -p ${INSTALLER_DB_PATH}
     mkdir -p ${INSTALLER_BANK_PATH}/FrontEnd/HiRes
     mkdir -p ${INSTALLER_BANK_PATH}/FrontEnd/LowRes
+    echo
 }
 
 makeDirectoriesForFull() {
     makeDirectories
-    mkdir -p ${BUILD_PATH}/FrontEnd/AllRes
-    mkdir -p ${BUILD_PATH}/FrontEnd/HiRes
-    mkdir -p ${BUILD_PATH}/FrontEnd/LowRes
-    mkdir -p ${BUILD_PATH}/Level/Hawai
+    mkdir -p ${BUILD_BANK_PATH}/FrontEnd/AllRes
+    mkdir -p ${BUILD_BANK_PATH}/FrontEnd/HiRes
+    mkdir -p ${BUILD_BANK_PATH}/FrontEnd/LowRes
+    mkdir -p ${BUILD_BANK_PATH}/Level/Hawai
+    echo
 }
