97c2616bd755fd4bd24053d2064c0aed36aa769f
djey <j.canler@gmail.com>
2016-05-30 13:29:42 +0200

    feature(scripts) : [] do not rebuild and copy scripts jar when already existing

--- scripts/build/util/win.sh
+++ scripts/build/util/win.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 installerScript() {
-    # TODO do not rebuild and copy jar if already present
     echo "*** Windows installer Script and libs... ***"
-
-    cd ${TDUCP_PATH}/tducp-scripts && ./gradlew fatJar && cd -
-
     cp ${SCRIPTS_PATH}/install-win/* ${BUILD_PATH}
-    cp ${TDUCP_PATH}/tducp-scripts/build/libs/*.jar ${INSTALLER_LIB_PATH}
+
+    # Do not rebuild and copy jar if already present
+    if [ ! -n "$(find ${INSTALLER_LIB_PATH} -name '*.jar')" ];
+    then
+        cd ${TDUCP_PATH}/tducp-scripts && ./gradlew fatJar && cd -
+        cp ${TDUCP_PATH}/tducp-scripts/build/libs/*.jar ${INSTALLER_LIB_PATH}
+    fi
 
     echo
 }
