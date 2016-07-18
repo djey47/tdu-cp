ab461b5ac1c4b6f6553bf0158a2867f9df3d3c5b
djey <j.canler@gmail.com>
2016-07-18 14:08:56 +0200

    feature(scripts) : [UPDATE] delete update patch for PNJ topic (unhandled)

--- tducp-scripts/src/main/shell/database/updateDiff.sh
+++ tducp-scripts/src/main/shell/database/updateDiff.sh
@@ -9,3 +9,8 @@ REFERENCE_DB_PATH=${TDUCP_PATH}/database/reference/1.66A_MP
 
 echo "Getting diffs between current database and 1.66MP one, please wait..."
 ../tduf/databaseTool.sh diff-patches -n -j ${CURRENT_DB_PATH} -J ${REFERENCE_DB_PATH} -p ${DIFF_PATCHES_PATH}
+
+echo "Deleting diff on PNJ topic..."
+rm ${DIFF_PATCHES_PATH}/PNJ.mini.json
+
+echo "All done!"
