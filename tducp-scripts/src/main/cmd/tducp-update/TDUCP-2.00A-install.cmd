730793a87d90ee164d272c55c4d13151e90bedf9
djey <j.canler@gmail.com>
2016-07-18 16:05:03 +0200

    feature(scripts) : [CLEAN] remove install files on success

--- tducp-scripts/src/main/cmd/tducp-update/TDUCP-2.00A-install.cmd
+++ tducp-scripts/src/main/cmd/tducp-update/TDUCP-2.00A-install.cmd
@@ -28,7 +28,12 @@ ECHO.
 MKDIR logs 2>NUL
 
 java -cp "%TDUCP_SCRIPTS_LIB%" fr.tduf.tducp.scripts.install.Update > logs\TDUCP-install.log 2>&1
-IF ERRORLEVEL 1 ECHO .Installation failed!
+IF ERRORLEVEL 1 (
+    SET TDUCP_FAIL=1
+    ECHO .Installation failed!
+) ELSE (
+    ECHO .Installation succeeded!
+)
 
 ECHO.
 ECHO .Installation details in logs\TDUCP-install.log file (will be displayed below).
@@ -50,12 +55,11 @@ ECHO.
 REM Removal of obsolete files
 DEL TDUCP-no
 DEL TDUCP-util.cmd
-DEL changelog.md
-DEL files.md
-DEL version.md
-DEL readme-tducp-full.md
-DEL readme-tducp-update.md
-REM TODO
-REM RMDIR %TDUCP_DIRECTORY% /S /Q
-REM DEL TDUCP-2.00A-install.cmd
-REM Should be the last command line of the file!
+DEL *.md
+IF NOT DEFINED TDUCP_FAIL (
+    REM Removal of install files
+    RMDIR %TDUCP_DIRECTORY% /S /Q
+    DEL TDUCP-2.00A-install*.cmd
+) ELSE (
+    REM Should be the last command line of the file!
+)
