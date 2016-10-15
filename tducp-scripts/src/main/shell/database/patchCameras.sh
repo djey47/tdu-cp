65e1497a4fc67e390b9c3caa162a82a9318c116b
djey <j.canler@gmail.com>
2016-10-15 17:27:33 +0200

    refactor(scripts/cameras) : [ULTICAM] use batch operation to clone all camera sets

--- tducp-scripts/src/main/shell/database/patchCameras.sh
+++ tducp-scripts/src/main/shell/database/patchCameras.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env bash
 #set -x
 
-# Add dedicated camera sets for all genuine/unlocked vehicle slots
+# Add dedicated camera sets for all genuine/unlocked/new vehicle slots
 
 source ../setEnv.sh
 
 SOURCE_CAMERAS=${TDUCP_PATH}/resources/physics/cameras/1.68B/cameras.bin
 ULTIMATE_CAMERAS=${TDUCP_PATH}/resources/physics/cameras/cameras.bin
 TMP_CAMERAS=/tmp/cameras.bin
+TMP_BATCH=/tmp/cameras.txt
 
 cp ${SOURCE_CAMERAS} ${TMP_CAMERAS}
+cp ./cameras.txt ${TMP_BATCH}
 
 echo "Applying camera patches from ${SOURCE_CAMERAS} to ${ULTIMATE_CAMERAS}, please wait..."
-cat cameras.txt | while read CAM
-do
-    IFS=';' read -r -a array <<< ${CAM}
-    SRC_CAM=${array[0]}
-    DEST_CAM=${array[1]}
-
-    echo "-> Base camera: ${SRC_CAM} to ${DEST_CAM}"
-    ../tduf/cameraTool.sh copy-set -n -i "${TMP_CAMERAS}" -o "${TMP_CAMERAS}" -s ${SRC_CAM} -t ${DEST_CAM}
-done
+../tduf/cameraTool.sh copy-sets -n -i "${TMP_CAMERAS}" -o "${TMP_CAMERAS}" -b "${TMP_BATCH}"
 
 cp ${TMP_CAMERAS} ${ULTIMATE_CAMERAS}
